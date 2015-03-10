741dfc2cf01bf75724a68708848e59519ee9683e
majid.sadeghi.alavijeh@gmail.com <majid.sadeghi.alavijeh@gmail.com>
2015-03-10 15:32:52 +0330

    WCF based on WSDL and SOAP web service for insert records in the third party database

---
+++ ITCSService.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace TCSAService
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the interface name "ITCSService" in both code and config file together.
+    [ServiceContract]
+    public interface ITCSService
+    {
+
+        [OperationContract]
+        void InsertTraffic(String trafficId, String takenDate, String takenTime, String persianPlate, String agentCode);
+
+        [OperationContract]
+        void updateTraffic(String trafficId, String persianPlate);
+
+        [OperationContract]
+        void changeTrafficStatus(String trafficId, Boolean status);
+    }
+}
---
+++ TCSService.svc.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+using TCSAService.Model;
+
+namespace TCSAService
+{
+       public class TCSService : ITCSService
+    {
+
+        public void InsertTraffic(string trafficId, string takenDate, string takenTime, string persianPlate, string agentCode)
+        {
+            try
+            {
+                DatabaseDataContext db = new DatabaseDataContext();
+                if (db.Traffics.Count(P => P.recordId.Equals(trafficId)) == 0)
+                {
+                    Traffic traffic = new Traffic();
+                    traffic.agentId = agentCode;
+                    traffic.recordDate = takenDate;
+                    traffic.recordTime = takenTime;
+                    traffic.detectedPlatePersian = persianPlate;
+                    traffic.insertDate = DateTime.Now;
+                    traffic.state = true;
+                    db.Traffics.InsertOnSubmit(traffic);
+                    db.SubmitChanges();
+                }
+                else
+                {
+                    OutgoingWebResponseContext response = WebOperationContext.Current.OutgoingResponse;
+                    response.StatusCode = HttpStatusCode.Found;
+                    response.StatusDescription = "traffic with specific id has been added before";
+                }
+            }
+            catch (Exception ex)
+            {
+                OutgoingWebResponseContext response = WebOperationContext.Current.OutgoingResponse;
+                response.StatusCode = HttpStatusCode.InternalServerError;
+                response.StatusDescription = ex.Message;
+            }
+        }
+
+        public void updateTraffic(string trafficId, string persianPlate)
+        {
+            try
+            {
+                DatabaseDataContext db = new DatabaseDataContext();
+                if (db.Traffics.Count(P => P.recordId.Equals(trafficId)) > 0)
+                {
+                    Traffic traffic = db.Traffics.Single(P => P.recordId.Equals(trafficId));
+                    ActionLog actionLog = new ActionLog();
+                    actionLog.action = "update";
+                    actionLog.date = DateTime.Now;
+                    actionLog.recordId = traffic.Id ;
+                    actionLog.lastValue = traffic.detectedPlatePersian;
+                    db.ActionLogs.InsertOnSubmit(actionLog);
+                    traffic.detectedPlatePersian = persianPlate;
+                    traffic.lastModifiedDate = DateTime.Now;
+                    db.SubmitChanges();
+                }
+                else
+                {
+                    OutgoingWebResponseContext response = WebOperationContext.Current.OutgoingResponse;
+                    response.StatusCode = HttpStatusCode.NoContent;
+                    response.StatusDescription = "traffic with specific id does not exist";
+                }
+            }
+            catch (Exception ex)
+            {
+                OutgoingWebResponseContext response = WebOperationContext.Current.OutgoingResponse;
+                response.StatusCode = HttpStatusCode.InternalServerError;
+                response.StatusDescription = ex.Message;
+            }
+        }
+
+
+        public void changeTrafficStatus(string trafficId, bool status)
+        {
+            try
+            {
+                DatabaseDataContext db = new DatabaseDataContext();
+                if (db.Traffics.Count(P => P.recordId.Equals(trafficId)) > 0)
+                {
+                    Traffic traffic = db.Traffics.Single(P => P.recordId.Equals(trafficId));
+                    ActionLog actionLog = new ActionLog();
+                    actionLog.action = "change state";
+                    actionLog.date = DateTime.Now;
+                    actionLog.recordId = traffic.Id;
+                    actionLog.lastValue = String.Format("{0}", traffic.state);
+                    db.ActionLogs.InsertOnSubmit(actionLog);
+                    traffic.state = status;
+                    traffic.lastModifiedDate = DateTime.Now;
+                    db.SubmitChanges();
+                }
+                else
+                {
+                    OutgoingWebResponseContext response = WebOperationContext.Current.OutgoingResponse;
+                    response.StatusCode = HttpStatusCode.NoContent;
+                    response.StatusDescription = "traffic with specific id does not exist";
+                }
+            }
+            catch (Exception ex)
+            {
+                OutgoingWebResponseContext response = WebOperationContext.Current.OutgoingResponse;
+                response.StatusCode = HttpStatusCode.InternalServerError;
+                response.StatusDescription = ex.Message;
+            }
+        }
+    }
+}
